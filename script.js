162993b6c1c3c0606aed062bed626e06cce9aace
Aaron Dang <aarondang4500@gmail.com>
2024-05-29 15:44:51 -0500

    Submit button now uses form data to create cards

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const modal = document.getElementById("modal");
-const confirm = document.getElementById("confirm");
-const addButton = document.getElementById("addButton");
-const cancel = document.querySelector(".cancel");
-
+// Initialize library array to store Book objects
 const myLibrary = [];
 
+// Function used to create Book objects
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -12,18 +9,63 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
-    myLibrary.push(book);
+// Render function creates cards when called
+let i = 0
+function render() {
+    let library = document.querySelector(".library");
+    let book = myLibrary[i];
+    let card = document.createElement("div");
+    let cardRead;
+
+    if (book.read == true) {
+        cardRead = "Has Been Read"
+    }
+    else {
+        cardRead = "Not Read Yet"
+    }
+
+    card.innerHTML = `<div class="innerCard">
+                            <strong>${book.title}</strong>
+                            <div>By <strong>${book.author}</strong></div>
+                            <div># of Pages: <strong>${book.pages}</strong></div>
+                            <div>Status: <strong>${cardRead}</strong></div>
+                        </div>
+                        <div class="remove">Remove</div>`
+
+    card.className = "card";
+    library.appendChild(card);
+    i += 1;
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet");
-console.log(myLibrary[0].title);
+// Adds Book objects to the library array 
+function addBookToLibrary() {
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").checked;
 
+    const book = new Book(title, author, pages, read);
+    console.log(book);
+    myLibrary.push(book);
+    render();
+}
+
+// Modal pops up when add book button is clicked
+const modal = document.getElementById("modal");
+const addButton = document.querySelector("#addButton");
 addButton.addEventListener("click", () => {
     modal.showModal();
 })
 
+// Modal is closed when the x button is clicked 
+const cancel = document.querySelector(".cancel");
 cancel.addEventListener("click", () => {
     modal.close();
+})
+
+// Submit button calls the addBookToLibrary function
+const form = document.querySelector("#bookForm");
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    addBookToLibrary();
 })
